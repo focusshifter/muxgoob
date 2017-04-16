f7838042f1f316c5d48c843696d27b384b467d07
Victor Shcherbakov <me@v1x.info>
2017-04-16 14:13:38 +0300

    Allow to react to the questions and printing 'chat specs'

--- plugins/reply/reply.go
+++ plugins/reply/reply.go
@@ -2,6 +2,8 @@ package reply
 
 import (
 	"regexp"
+	"math/rand"
+	"time"
 
 	"github.com/tucnak/telebot"
 	"github.com/asdine/storm"
@@ -13,6 +15,7 @@ type ReplyPlugin struct {
 }
 
 var db *storm.DB
+var rng *rand.Rand
 
 func init() {
 	registry.RegisterPlugin(&ReplyPlugin{})
@@ -20,15 +23,40 @@ func init() {
 
 func (p *ReplyPlugin) Start(sharedDb *storm.DB) {
 	db = sharedDb
+    rng = rand.New(rand.NewSource(time.Now().UnixNano()))
 }
 
 func (p *ReplyPlugin) Run(message telebot.Message) {
+	bot := registry.Bot
+
+	techExp := regexp.MustCompile(`(?i)^\!ттх$`)
+	questionExp := regexp.MustCompile(`^.*(gooby|губи|губ(я)+н).*\?$`)
 	highlightedExp := regexp.MustCompile(`^.*(gooby|губи|губ(я)+н).*$`)
 
-	if highlightedExp.MatchString(message.Text) {
-		bot := registry.Bot
+	switch {
+		case techExp.MatchString(message.Text):
+			bot.SendMessage(message.Chat,
+						"ТТХ: https://drive.google.com/open?id=139ZWbP-CAV_u5nzQ6skbHRjb7eofzfdh8eA4_q7McFM",
+						&telebot.SendOptions{DisableWebPagePreview: true, DisableNotification: true})
 
-		bot.SendMessage(message.Chat, "herp derp", nil)
+		case questionExp.MatchString(message.Text):
+			var replyText string
+
+			rngInt := rng.Int()
+
+			switch {
+				case rngInt % 100 == 0:
+					replyText = "Заткнись, пидор"
+				case rngInt % 2 == 0:
+					replyText = "Да"
+				default:
+					replyText = "Нет"
+			}
+			
+			bot.SendMessage(message.Chat, replyText, &telebot.SendOptions{ReplyTo: message})
+
+		case highlightedExp.MatchString(message.Text):	
+			bot.SendMessage(message.Chat, "herp derp", nil)
 	}
 }
 
